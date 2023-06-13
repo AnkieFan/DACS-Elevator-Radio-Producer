e1550c5ed270ebc755626e13adffedaf225cdb8e
AnkieFan <72519414+AnkieFan@users.noreply.github.com>
2023-06-13 14:11:22 +0200

    Update spotify API

--- data/from_spotify.py
+++ data/from_spotify.py
@@ -1,8 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = "a2a53441b6384cd6886acffc758b11d8"
-client_secret = "3b47ba1e5f024acc8306fa7514e57808"
+client_id = "Your Client ID"
+client_secret = "Your Client Secret"
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
